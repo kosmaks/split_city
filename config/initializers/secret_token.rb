c971f49ae60d811e36c6274376a5e5606da195f3
Maxim Kostuchenko <kstmaks@gmail.com>
2014-02-26 10:23:20 +0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gis::Application.config.secret_key_base = '8f19afb8131b09179d51399fa65d84adea68b64050a1b5206383452d6272be556756a0d2eac2a01fade42be2bce45d25111eb3fcc53b9b5eb886f0ced16b7dc2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gis::Application.config.session_store :cookie_store, key: '_gis_session'
