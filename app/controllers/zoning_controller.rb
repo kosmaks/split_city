0868cb524c57bbf9f196e830650f8c39d0bd8e3c
Maxim Kostuchenko <kstmaks@gmail.com>
2014-03-20 23:01:40 +0600

    Calculate center

--- app/controllers/zoning_controller.rb
+++ app/controllers/zoning_controller.rb
@@ -2,7 +2,7 @@ class ZoningController < ApplicationController
   def debug
     venues = Venue.all.limit(10).to_a
 
-    result = Clust::FCM::run
+    result = Clust::FCM.new([1, 2, 7, 8, 9], 2).run
 
     respond_to do |format|
       format.json { render json: result }
--- lib/clust/fcm.rb
+++ lib/clust/fcm.rb
@@ -1,29 +1,66 @@
+require 'matrix'
+
+# main module
+
 module Clust
   class FCM
+
     class Point
-      def initialize data, num_of_clusters
-        @num_of_clusters = num_of_clusters
-        @data = data
-        @coef = randomize
+      attr_reader :point
+      attr_accessor :coef
+
+      def initialize point, coef
+        @point = Vector.elements [point].flatten
+        @coef  = coef
       end
 
-      private
+      def u k
+        @coef[k]
+      end
 
-      def randomize
-        sum = 1000.0
-        res = []
-        @num_of_clusters.times do |x| 
-          perc = rand sum
-          perc = (perc > sum or x == @num_of_clusters) ? sum : perc
-          res << perc
-          sum -= perc
-        end
-        res.map { |x| x / 1000.0 }
+      def update_u k, center
+      end
+
+      def dist point
+        Math.sqrt(
+          @point.zip(point)
+                .map { |x, y| (x - y) ** 2 }
+                .sum
+        )
       end
     end
 
-    def self.run
-      [Point.new([1, 2, 3], 10)]
+    attr_reader :num_of_clust, :points
+
+    def initialize data, num_of_clust, params={}
+      @m = params[:m] || 2
+      @num_of_clust = num_of_clust
+      @points = data.map { |x| Point.new x, randomize }
+    end
+
+    def run
+      center 0
+    end
+
+    private
+
+    def center k
+      weights = points.map { |p| p.u(k) ** @m }.sum
+      points.map { |p| p.point * (p.u(k) ** @m) }
+            .inject(:+) / weights
+    end
+
+    def randomize
+      sum = 1000.0
+      res = []
+      (num_of_clust - 1).times do |x| 
+        perc = rand sum
+        perc = (perc > sum) ? sum : perc
+        res << perc
+        sum -= perc
+      end
+      res << sum
+      res.map { |x| x / 1000.0 }
     end
   end
 end
